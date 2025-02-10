b4cf17dbc5a15ca8c016d3e1cad59c6f79154ad0
Natthaphon Sutinan <natthaphonsu0535@gmail.com>
2025-02-10 21:03:26 +0700

    setup project

---
+++ Web-Ecommerce/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Ecommerce.Models;
+
+namespace Web_Ecommerce.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
