a7e25e158492cb50c2f24159156d7ea0af1157cf
Natthaphon Sutinan <natthaphonsu0535@gmail.com>
2025-02-16 21:59:07 +0700

    learn create contorller create view and setting layout

---
+++ Web-Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Ecommerce.Data;
+using Web_Ecommerce.Models;
+namespace Web_Ecommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
