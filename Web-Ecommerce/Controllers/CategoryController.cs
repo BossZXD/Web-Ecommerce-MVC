3af0f59d20a0e9f7dd00fa464ec28218b9c126f1
Natthaphon Sutinan <natthaphonsu0535@gmail.com>
2025-02-23 22:34:45 +0700

    Delete and TempData

--- Web-Ecommerce/Controllers/CategoryController.cs
+++ Web-Ecommerce/Controllers/CategoryController.cs
@@ -15,7 +15,19 @@ namespace Web_Ecommerce.Controllers
             List<Category> objCategoryList = _db.Categories?.ToList() ?? new List<Category>();
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+            return View(new Category());
+        }
 
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Create Successfully";
+            return RedirectToAction("Index");
+        }
         public IActionResult Edit(int? id)
         {
             if (id == null || id == 0)
@@ -39,19 +51,36 @@ namespace Web_Ecommerce.Controllers
         {
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Update Successfully";
+
             return RedirectToAction("Index");
         }
 
-        public IActionResult Create()
+        public IActionResult Delete(int? id)
         {
-            return View(new Category());
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(category);
         }
 
         [HttpPost]
-        public IActionResult Create(Category obj)
+        public IActionResult Delete(Category obj)
         {
-            _db.Categories.Add(obj);
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category Delete Successfully";
+
             return RedirectToAction("Index");
         }
     }
