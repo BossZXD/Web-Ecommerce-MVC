6b61726b196eba7581bfa434716241206f4e26ab
Natthaphon Sutinan <natthaphonsu0535@gmail.com>
2025-02-23 22:12:14 +0700

    edit update

--- Web-Ecommerce/Controllers/CategoryController.cs
+++ Web-Ecommerce/Controllers/CategoryController.cs
@@ -16,6 +16,32 @@ namespace Web_Ecommerce.Controllers
             return View(objCategoryList);
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.Find(id);
+
+            if(category == null)
+            {
+                return NotFound();
+
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Create()
         {
             return View(new Category());
